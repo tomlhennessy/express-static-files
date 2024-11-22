bfd16925d776bb134053719b941e576e64ac915d
Tom Hennessy <tomlhennessy@outlook.com>
2024-11-22 10:06:39 +1000

    serving static files

--- server/app.js
+++ server/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
-// Your code here
+
+// part 1: serve all files in the `assets`folder
+// app.use(express.static('server/assets'));
+
+// part 2: serve only files in the `assets/scripts` folder at the root URL
+// app.use(express.static('server/assets/scripts'));
+
+// part 3: serve files in the `assets/css` folder under a `stylesheets` route
+// app.use('/stylesheets', express.static('server/assets/css'))
+
+// bonus: serve files in `assets/images` folder under a `stickers` route
+app.use('/stickers', express.static('server/assets/images'));
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
